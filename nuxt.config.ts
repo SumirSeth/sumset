5508c2625961cf268ec29d022dd06a65e35281f2
Sumir Seth <iamsumirseth@gmail.com>
2025-04-15 20:44:19 +0530

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+ css: ['~/assets/css/main.css'],
+                    vite: {
+                      plugins: [
+                        tailwindcss(),
+                        ],
+                    },
+  compatibilityDate: "2025-04-15",
+  devtools: { enabled: true },
+  modules: ['@nuxt/fonts', '@nuxt/icon']
+})
