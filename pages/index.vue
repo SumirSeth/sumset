fbf5ff8a37083ae9eea96684f668af9638d62fd8
Sumir Seth <iamsumirseth@gmail.com>
2025-04-15 23:16:25 +0530

    chore: cleaner layout to see theme use.

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,46 @@
-<!-- pages/theme-test.vue -->
 <template>
-  <div>
-    <div class="h-screen theme-box">
-      <h1 class="text-xl font-bold mb-4">Theme Tester</h1>
-      
-      <select v-model="selected" @change="setTheme(selected)" class="border px-2 py-1 rounded">
-        <option value="auto">Auto</option>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-        <option value="nord">Nord</option>
-      </select>
-      
-      <p class="mt-4 text-[var(--text)] bg-[var(--bg)] p-4 rounded">
-        The current theme is <strong>{{ theme }}</strong>
-      </p>
+  <div class=" theme-box flex flex-col items-center justify-center min-h-screen">
+    <div class="flex flex-col items-center justify-center">
+      <div class="flex flex-col items-center justify-center">
+        <h1 class="text-3xl font-bold">Sumset</h1>
+        <p class="text-lg">A note taking app</p>
+      </div>
+      <div class="flex flex-col items-center justify-center">
+        <div class="flex flex-col items-center justify-center">
+          <button class="theme-box" @click="setTheme('light')">
+            <div class="flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+              </svg>
+            </div>
+            <div class="text-center">Light</div>
+          </button>
+          <button class="theme-box" @click="setTheme('dark')">
+            <div class="flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+              </svg>
+            </div>
+            <div class="text-center">Dark</div>
+          </button>
+          <button class="theme-box" @click="setTheme('nord')">
+            <div class="flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+              </svg>
+            </div>
+            <div class="text-center">Nord</div>
+          </button>
+          <button class="theme-box" @click="setTheme('auto')">
+            <div class="flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+              </svg>
+            </div>
+            <div class="text-center">Auto</div>
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,6 +48,6 @@
 <script setup lang="ts">
 
 const { theme, setTheme } = useTheme()
-
 const selected = ref(theme.value)
+
 </script>
