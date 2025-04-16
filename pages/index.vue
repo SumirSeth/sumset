994396ccd941aa5337d8608103c360ef67723864
Sumir Seth <iamsumirseth@gmail.com>
2025-04-16 21:21:13 +0530

    feat: add transition effects to textarea and update theme styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="theme-box flex-grow min-h-[90dvh] flex flex-col items-center justify-center bg-gray-700 p-4">
-    <textarea class="w-11/12 flex-grow flex flex-col items-center justify-center bg-gray-800 rounded-lg max-h-[90vh]">
+    <textarea class="txtarea w-11/12 flex-grow flex flex-col items-center justify-center bg-gray-800 rounded-lg max-h-[90vh]">
       
     </textarea>
   </div>
